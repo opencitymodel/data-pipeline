65958d0c9a072cc8a6f01e764ed0dbb722799b2f
Allen Gilliland <agilliland@gmail.com>
2018-08-29 15:13:50 -0700

    update grid processing code to adding in county geocoding and attach that attribute to our gridded footprints data.

--- footprints/prep-msfp/grid-and-attrs/app.js
+++ footprints/prep-msfp/grid-and-attrs/app.js
@@ -4,13 +4,24 @@ geolib = require('geolib');
 mkdirp = require('mkdirp');
 mgrs = require('mgrs');
 OpenLocationCode = require('open-location-code').OpenLocationCode;
+pointInPolygon = require('point-in-polygon');
+readline = require('readline');
 _ = require('underscore');
+
+stateCodes = require('./state-codes')
+
 
 
 // we must have a file for us to process
 if (process.argv.length < 3) {
-    console.log("error: no file specified for processing");
-    console.log("usage: node app.js <file-to-process>");
+    console.log("error: no state specified for processing");
+    console.log("usage: node app.js <state-name>");
+    return;
+}
+
+// we must have an environment variable telling us where to load our input files from
+if (!process.env.OCM_RAWFILES) {
+    console.log("error: environment variable 'OCM_RAWFILES' is not set.  this is required.");
     return;
 }
 
@@ -21,12 +32,13 @@ if (!process.env.OCM_GRIDFILES) {
 }
 
 
-const INPUT_FILE = process.argv[2];
+const STATE = process.argv[2];
+const FP_RAW_FOLDER = process.env.OCM_RAWFILES;
 const FP_GRID_FOLDER = process.env.OCM_GRIDFILES;
 
-// extract the filename from the infile
-// NOTE: this assumes unix style paths and filenames with no '.' characters in them
-const FP_STATE = INPUT_FILE.split('/').pop().split('.')[0];
+const COUNTY_SHAPES_FILE = FP_RAW_FOLDER+"/county.geo.txt";
+const COUNTY_MGRS_MAPPING_FILE = FP_RAW_FOLDER+"/"+STATE+"-mgrs-to-counties.txt";
+const BUILDING_FOOTPRINTS = FP_RAW_FOLDER+"/"+STATE+".txt";
 
 // this breaks a 1km MGRS identifier into 3 parts
 const MGRS_REGEX = /([0-9A-Z]+)([A-Z]{2})([0-9]{4})/
@@ -37,7 +49,7 @@ function writeFootprint(mgrsGrid, building) {
     const match = MGRS_REGEX.exec(mgrsGrid);
     const gzd = match[1];
     const gsid = match[2];
-    const outpath = FP_STATE+"/"+gzd+"/"+gsid;
+    const outpath = STATE+"/"+gzd+"/"+gsid;
 
     // make sure our folder path exists, otherwise we can't open up the actual files
     mkdirp(FP_GRID_FOLDER+"/"+outpath, function(err) {
@@ -104,47 +116,153 @@ function ubid(center, northeast, southwest) {
 }
 
 
-// open up our input file and start reading line by line
-const stream = byline(fs.createReadStream(INPUT_FILE, { encoding: "utf-8"}));
+function loadCountyShapes() {
+    const countyShapes = {};
 
-stream.on("data", function(line) {
-    const footprint = JSON.parse(line);
-
-    // hash the geometry coordinates into a unique signature for the building
-    const hashStr = footprint.geometry.coordinates[0].reduce(function (acc, val) {
-        return acc + val;
-    }, "");
-    const signature = quickHash(hashStr);
-
-    // calculate centroid
-    const ctr = geolib.getCenter(footprint.geometry.coordinates[0].map(p => {
-        return { longitude: p[0], latitude: p[1] }
-    }));
-    // not sure why, but the centroid funtion is returning values as strings =()
-    const center = { lat: parseFloat(ctr.latitude), lon: parseFloat(ctr.longitude) }
-
-    // calculate bounding box
-    const bbox = geolib.getBounds(footprint.geometry.coordinates[0].map(p => {
-        return { longitude: p[0], latitude: p[1] }
-    }));
-
-    // TODO: calculate area
-
-    // calculate MGRS grid @ 1km resolution
-    const mgrsGrid = mgrs.forward([center.lon, center.lat], 2);
-
-    // calculate UBID for the footprint
-    const bbox_ne = { latitude: bbox.maxLat, longitude: bbox.maxLng }
-    const bbox_sw = { latitude: bbox.minLat, longitude: bbox.minLng }
-    const ubid = ubid(center, bbox_ne, bbox_sw);
-
-    // add the footprint to output file
-    writeFootprint(mgrsGrid, {
-        sig: signature,
-        ubid,
-        msfp_st: FP_STATE,
-        mgrs: mgrsGrid,
-        ctr: center,
-        fp: footprint
+    // open up our input file and start reading line by line
+    const stream = readline.createInterface({
+        input: fs.createReadStream(COUNTY_SHAPES_FILE, { encoding: "utf-8"})
     });
-});
+
+    stream.on("line", function(line) {
+        if (line.endsWith(",")) {
+            line = line.substring(0, line.length - 1);
+        }
+        const countyDef = JSON.parse(line);
+
+        // extract a couple things
+        const stateCode = countyDef.properties.STATEFP;
+        const stateName = stateCodes[stateCode].name;
+        const msfpStateName = stateName.replace(/ /g, "");
+
+        // we only care about the counties for the state we are processing
+        if (msfpStateName === STATE) {
+            countyShapes[countyDef.properties.GEOID] = countyDef.geometry.coordinates[0];
+        }
+    });
+
+    stream.on("close", () => {
+        console.log("finished loading county shapes");
+
+        // next load the mgrs->county mapping
+        loadMgrsToCountyMapping(countyShapes);
+    });
+}
+
+function loadMgrsToCountyMapping(countyShapes) {
+    const mgrsToCountyMapping = {};
+
+    // open up our input file and start reading line by line
+    const stream = readline.createInterface({
+        input: fs.createReadStream(COUNTY_MGRS_MAPPING_FILE, { encoding: "utf-8"})
+    });
+
+    stream.on("line", function(line) {
+        const mapping = JSON.parse(line);
+        mgrsToCountyMapping[mapping.mgrs] = mapping.counties;
+    });
+
+    stream.on("close", () => {
+        console.log("finished loading mgrs->county mapping");
+
+        // now its time to process the building footprints
+        processFootprints(countyShapes, mgrsToCountyMapping);
+    });
+}
+
+function processFootprints(countyShapes, mgrsToCountyMapping) {
+    // open up our input file and start reading line by line
+    const stream = readline.createInterface({
+        input: fs.createReadStream(BUILDING_FOOTPRINTS, { encoding: "utf-8"})
+    });
+
+    const missingCounty = {};
+
+    stream.on("line", function(line) {
+        const footprint = JSON.parse(line);
+
+        // hash the geometry coordinates into a unique signature for the building
+        const hashStr = footprint.geometry.coordinates[0].reduce(function (acc, val) {
+            return acc + val;
+        }, "");
+        const signature = quickHash(hashStr);
+
+        // calculate centroid
+        const ctr = geolib.getCenter(footprint.geometry.coordinates[0].map(p => {
+            return { longitude: p[0], latitude: p[1] }
+        }));
+        // not sure why, but the centroid funtion is returning values as strings =(
+        const center = { lat: parseFloat(ctr.latitude), lon: parseFloat(ctr.longitude) }
+
+        // calculate bounding box
+        const bbox = geolib.getBounds(footprint.geometry.coordinates[0].map(p => {
+            return { longitude: p[0], latitude: p[1] }
+        }));
+
+        // TODO: calculate area
+
+        // calculate MGRS grid @ 1km resolution
+        const mgrsGrid = mgrs.forward([center.lon, center.lat], 2);
+
+        // calculate UBID for the footprint
+        const bbox_ne = { latitude: bbox.maxLat, longitude: bbox.maxLng }
+        const bbox_sw = { latitude: bbox.minLat, longitude: bbox.minLng }
+        const bid = ubid(center, bbox_ne, bbox_sw);
+
+        // reverse geocode to determine county
+        let countyId = null;
+        const possibleCounties = mgrsToCountyMapping[mgrsGrid];
+        if (possibleCounties && possibleCounties.length > 0) {
+            // if there is only 1 county for this grid then we are done
+            if (possibleCounties.length === 1) {
+                countyId = possibleCounties[0];
+
+            // otherwise, we need to use the county shape files to determine the county for this building
+            } else {
+                countyId = _.find(possibleCounties, county => {
+                    return pointInPolygon([center.lon, center.lat], countyShapes[county])
+                });
+            }
+        } else {
+            // TODO: if possibleCounties is non-existant then assume our prep job missed this grid and we need
+            //      to do the work right now
+            countyId = _.find(Object.keys(countyShapes), county => {
+                return pointInPolygon([center.lon, center.lat], countyShapes[county])
+            });
+
+            if (countyId) {
+                console.log("MISSING_MATCHED", mgrsGrid, countyId, center.lat+","+center.lon);
+            } else {
+                console.log("NO_COUNTY", mgrsGrid, center.lat+","+center.lon);
+            }
+
+            if (!missingCounty[mgrsGrid]) {
+                missingCounty[mgrsGrid] = [];
+            }
+
+            missingCounty[mgrsGrid].push(center);
+        }
+
+        // add the footprint to output file
+        writeFootprint(mgrsGrid, {
+            sig: signature,
+            ubid: bid,
+            msfp_st: STATE,
+            mgrs: mgrsGrid,
+            ctr: center,
+            county: countyId,
+            fp: footprint
+        });
+    });
+
+    stream.on("close", () => {
+        const g = Object.keys(missingCounty).sort();
+        g.forEach(grid => {
+            console.log(grid, missingCounty[grid].length);
+        });
+        console.log(g.length);
+    });
+}
+
+// this kicks things off and runs through everything
+loadCountyShapes();
