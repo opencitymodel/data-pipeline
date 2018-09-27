1d5570274db5f1c94aacf411728a7766d348cd6e
Allen Gilliland <agilliland@gmail.com>
2018-09-27 11:58:03 -0700

    little bit of tidy up.

--- footprints/mgrs-to-county/map-state.sh
+++ footprints/mgrs-to-county/map-state.sh
@@ -2,8 +2,6 @@
 
 # These environment variables are expected to be set which determine what data we
 # are working on and where to store the results when we are done.
-# OCM_STATE="DistrictofColumbia"
-# OCM_GEOCODE_S3BUCKET="ocm-geocode-test"
 if [ -z ${OCM_STATE+x} ]; then
     echo "OCM_STATE is not set"
     exit 1
