0fca601ad7e5aa51b3a820f83fe548d33acc3d2f
Allen Gilliland <agilliland@gmail.com>
2018-12-14 14:24:12 -0800

    add a notion of a version to our citygml generation script, thus expecting that building data is versioned which carries over into the citygml.

--- citygml/build-citygml.sh
+++ citygml/build-citygml.sh
@@ -15,6 +15,9 @@ if [ -z ${OCM_COUNTY+x} ]; then
 elif [ -z ${OCM_STATE+x} ]; then
     echo "OCM_STATE is not set"
     exit 1
+elif [ -z ${OCM_VERSION+x} ]; then
+    echo "OCM_VERSION is not set"
+    exit 1
 elif [ -z ${OCM_BLDGS_S3BUCKET+x} ]; then
     echo "OCM_BLDGS_S3BUCKET is not set"
     exit 1
@@ -22,7 +25,7 @@ elif [ -z ${OCM_GML_S3BUCKET+x} ]; then
     echo "OCM_GML_S3BUCKET is not set"
     exit 1
 else
-    echo "Creating CityGML files for $OCM_STATE/$OCM_COUNTY and saving output to s3://$OCM_GML_S3BUCKET/"
+    echo "Creating CityGML files for $OCM_VERSION/$OCM_STATE/$OCM_COUNTY and saving output to s3://$OCM_GML_S3BUCKET/"
 fi
 
 
@@ -31,7 +34,7 @@ mkdir -p ${OCM_BLDGSFILES}
 mkdir -p ${OCM_GMLFILES}
 
 # download the buildings data from S3 which we will be transforming into citygml
-aws s3 cp s3://${OCM_BLDGS_S3BUCKET}/${OCM_STATE}/cty=${OCM_COUNTY} ${OCM_BLDGSFILES}/ --recursive
+aws s3 cp s3://${OCM_BLDGS_S3BUCKET}/${OCM_VERSION}/${OCM_STATE}/cty=${OCM_COUNTY} ${OCM_BLDGSFILES}/ --recursive
 
 # run our citygml builder
 OUTFILE_PREFIX="${OCM_STATE}-${OCM_COUNTY}"
@@ -39,7 +42,7 @@ java -server -jar citygml.jar ${OCM_BLDGSFILES} ${OCM_GMLFILES} ${OUTFILE_PREFIX
 
 # push the citygml files back to S3
 if [ $? -eq 0 ]; then
-    aws s3 cp ${OCM_GMLFILES} s3://${OCM_GML_S3BUCKET}/${OCM_STATE}/ --recursive
+    aws s3 cp ${OCM_GMLFILES} s3://${OCM_GML_S3BUCKET}/${OCM_VERSION}/${OCM_STATE}/ --recursive
 else
     exit 1
 fi
