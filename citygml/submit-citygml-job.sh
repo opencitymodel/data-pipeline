a384cb148c93640d835e8c57e85a9cd5a493c40f
Allen Gilliland <agilliland@gmail.com>
2019-01-07 12:30:30 -0800

    update wrapper scripts to account for new argument representing data format.

--- citygml/submit-citygml-job.sh
+++ citygml/submit-citygml-job.sh
@@ -3,16 +3,17 @@
 # Simple script which wraps the AWS cmd line tool and is meant for submitting
 # a job to generate citygml for a given state & county
 NAME="citygml"
-REVISION="4"
+REVISION="5"
 QUEUE="SmallBatchJobs"
 
 VERSION=$1
-STATE=$2
-COUNTY=$3
+FORMAT=$2
+STATE=$3
+COUNTY=$4
 DATE=`date +%s`
 JOB_NAME="${STATE}-${NAME}-${DATE}"
 JOB_DEF="${NAME}:${REVISION}"
-JOB_PROPS="{\"environment\":[{\"name\":\"OCM_STATE\",\"value\":\"${STATE}\"},{\"name\":\"OCM_COUNTY\",\"value\":\"${COUNTY}\"},{\"name\":\"OCM_VERSION\",\"value\":\"${VERSION}\"}]}"
+JOB_PROPS="{\"environment\":[{\"name\":\"OCM_STATE\",\"value\":\"${STATE}\"},{\"name\":\"OCM_COUNTY\",\"value\":\"${COUNTY}\"},{\"name\":\"OCM_VERSION\",\"value\":\"${VERSION}\"},{\"name\":\"OCM_FORMAT\",\"value\":\"${FORMAT}\"}]}"
 
 
 # TODO: we could potentially try to lookup the latest revision for the job def, or ask the user for it?
