5692229325f7cc8cfe7dd07b719fad6e38dfc0e4
Allen Gilliland <agilliland@gmail.com>
2019-05-31 15:35:13 -0700

    update to new job revision

--- geocode/grid-to-county/submit-mapping-job.sh
+++ geocode/grid-to-county/submit-mapping-job.sh
@@ -2,7 +2,7 @@
 
 # Simple script which wraps the AWS cmd line tool and is meant for submitting a job to aws batch
 NAME="grid-to-county"
-REVISION="3"
+REVISION="1"
 QUEUE="GeocodeJobs"
 
 STATE=$1
