7657390d40e37402229ea027533b4c90227090bd
Allen Gilliland <agilliland@gmail.com>
2019-05-31 08:39:55 -0700

    separate out our attributes calcs into its own function so we can add unit tests :)

--- footprints/grid-and-attrs/app.js
+++ footprints/grid-and-attrs/app.js
@@ -206,6 +206,102 @@ async function loadMgrsToCountyMapping (mgrsToCountyFile) {
   })
 }
 
+function processFootprint (footprint, state, countyShapes, mgrsToCountyMapping) {
+  // we don't support actual MultiPolygon shapes, so either bail or convert to Polygon if we can
+  if (footprint.geometry.type === 'MultiPolygon') {
+    if (footprint.geometry.coordinates.length > 1) {
+      throw new Error('UNSUPPORTED_MULTIPOLYGON')
+    } else {
+      // convert to Polygon given this isn't really a MultiPolygon
+      footprint.geometry.type = 'Polygon'
+      footprint.geometry.coordinates = footprint.geometry.coordinates[0]
+    }
+  }
+
+  // validate and repair geometry if needed
+  try {
+    // const origGeometry = footprint.geometry
+    footprint.geometry = repair.repairGeometry(footprint.geometry)
+    // if (origGeometry !== footprint.geometry) repairedPolygons++
+  } catch (geomError) {
+    throw new Error('GEOMETRY_ERROR', geomError)
+  }
+
+  // hash the geometry coordinates into a unique signature for the building
+  const hashStr = footprint.geometry.coordinates[0].reduce(function (acc, val) {
+    return acc + val
+  }, '')
+  const signature = quickHash(hashStr)
+
+  // calculate centroid
+  const ctr = geolib.getCenter(footprint.geometry.coordinates[0].map(p => {
+    return { longitude: p[0], latitude: p[1] }
+  }))
+  // not sure why, but the centroid funtion is returning values as strings =(
+  const center = { lat: parseFloat(ctr.latitude), lon: parseFloat(ctr.longitude) }
+
+  // calculate bounding box
+  const bbox = geolib.getBounds(footprint.geometry.coordinates[0].map(p => {
+    return { longitude: p[0], latitude: p[1] }
+  }))
+
+  // calculate area
+  const turfPoly = turfHelpers.polygon(footprint.geometry.coordinates)
+  const area = turfArea.default(turfPoly)
+
+  // calculate MGRS grid @ 1km resolution
+  const mgrsGrid = mgrs.forward([center.lon, center.lat], 2)
+
+  // calculate UBID for the footprint
+  const bboxNortheast = { latitude: bbox.maxLat, longitude: bbox.maxLng }
+  const bboxSouthwest = { latitude: bbox.minLat, longitude: bbox.minLng }
+  const bid = ubid(center, bboxNortheast, bboxSouthwest)
+
+  // reverse geocode to determine county
+  let countyId = null
+  const possibleCounties = mgrsToCountyMapping[mgrsGrid]
+  if (possibleCounties && possibleCounties.length > 0) {
+    if (possibleCounties.length === 1) {
+      // there is only 1 county for this grid so we are done
+      countyId = possibleCounties[0]
+    } else {
+      countyId = _.find(possibleCounties, county => {
+        return pointInCounty([center.lon, center.lat], countyShapes[county])
+      })
+    }
+  } else {
+    // if possibleCounties is non-existant then we missed this grid and we need to figure it out right now
+    countyId = _.find(Object.keys(countyShapes), county => {
+      return pointInCounty([center.lon, center.lat], countyShapes[county])
+    })
+
+    if (countyId) {
+      console.log('MISSING_MATCHED', mgrsGrid, countyId, center.lat + ',' + center.lon)
+    } else {
+      console.log('NO_COUNTY', mgrsGrid, center.lat + ',' + center.lon)
+    }
+  }
+
+  // add all of our new properties, and rename all existing props to lowercase for consistency
+  let props = Object.keys(footprint.properties).reduce((c, k) => {
+    c = c[k.toLowerCase()] = footprint.properties[k]
+    return c
+  }, {})
+
+  footprint.properties = _.extend(props, {
+    sig: signature,
+    ubid: bid,
+    state,
+    county: countyId,
+    lat: center.lat,
+    lon: center.lon,
+    mgrs: mgrsGrid,
+    area
+  })
+
+  return footprint
+}
+
 function processFootprints (args, countyShapes, mgrsToCountyMapping) {
   const started = new Date()
   console.log('Starting ' + args.state + ' @', started)
@@ -228,106 +324,10 @@ function processFootprints (args, countyShapes, mgrsToCountyMapping) {
         line = line.substring(0, line.length - 1)
       }
 
-      const footprint = JSON.parse(line)
-
-      // we don't support actual MultiPolygon shapes, so either bail or convert to Polygon if we can
-      if (footprint.geometry.type === 'MultiPolygon') {
-        if (footprint.geometry.coordinates.length > 1) {
-          multiPolygons++
-          console.log('UNSUPPORTED_MULTIPOLYGON', line)
-          return
-        } else {
-          // convert to Polygon given this isn't really a MultiPolygon
-          footprint.geometry.type = 'Polygon'
-          footprint.geometry.coordinates = footprint.geometry.coordinates[0]
-        }
-      }
-
-      // validate and repair geometry if needed
-      try {
-        const origGeometry = footprint.geometry
-        footprint.geometry = repair.repairGeometry(footprint.geometry)
-        if (origGeometry !== footprint.geometry) repairedPolygons++
-      } catch (geomError) {
-        console.log('GEOMETRY_ERROR', geomError)
-        badPolygons++
-        return
-      }
-
-      // hash the geometry coordinates into a unique signature for the building
-      const hashStr = footprint.geometry.coordinates[0].reduce(function (acc, val) {
-        return acc + val
-      }, '')
-      const signature = quickHash(hashStr)
-
-      // calculate centroid
-      const ctr = geolib.getCenter(footprint.geometry.coordinates[0].map(p => {
-        return { longitude: p[0], latitude: p[1] }
-      }))
-      // not sure why, but the centroid funtion is returning values as strings =(
-      const center = { lat: parseFloat(ctr.latitude), lon: parseFloat(ctr.longitude) }
-
-      // calculate bounding box
-      const bbox = geolib.getBounds(footprint.geometry.coordinates[0].map(p => {
-        return { longitude: p[0], latitude: p[1] }
-      }))
-
-      // calculate area
-      const turfPoly = turfHelpers.polygon(footprint.geometry.coordinates)
-      const area = turfArea.default(turfPoly)
-
-      // calculate MGRS grid @ 1km resolution
-      const mgrsGrid = mgrs.forward([center.lon, center.lat], 2)
-
-      // calculate UBID for the footprint
-      const bboxNortheast = { latitude: bbox.maxLat, longitude: bbox.maxLng }
-      const bboxSouthwest = { latitude: bbox.minLat, longitude: bbox.minLng }
-      const bid = ubid(center, bboxNortheast, bboxSouthwest)
-
-      // reverse geocode to determine county
-      let countyId = null
-      const possibleCounties = mgrsToCountyMapping[mgrsGrid]
-      if (possibleCounties && possibleCounties.length > 0) {
-        if (possibleCounties.length === 1) {
-          // there is only 1 county for this grid so we are done
-          countyId = possibleCounties[0]
-        } else {
-          countyId = _.find(possibleCounties, county => {
-            return pointInCounty([center.lon, center.lat], countyShapes[county])
-          })
-        }
-      } else {
-        // if possibleCounties is non-existant then we missed this grid and we need to figure it out right now
-        countyId = _.find(Object.keys(countyShapes), county => {
-          return pointInCounty([center.lon, center.lat], countyShapes[county])
-        })
-
-        if (countyId) {
-          console.log('MISSING_MATCHED', mgrsGrid, countyId, center.lat + ',' + center.lon)
-        } else {
-          console.log('NO_COUNTY', mgrsGrid, center.lat + ',' + center.lon)
-        }
-      }
-
-      // add all of our new properties, and rename all existing props to lowercase for consistency
-      let props = Object.keys(footprint.properties).reduce((c, k) => {
-        c = c[k.toLowerCase()] = footprint.properties[k]
-        return c
-      }, {})
-
-      footprint.properties = _.extend(props, {
-        sig: signature,
-        ubid: bid,
-        state: args.state,
-        county: countyId,
-        lat: center.lat,
-        lon: center.lon,
-        mgrs: mgrsGrid,
-        area
-      })
+      const footprint = processFootprint(JSON.parse(line), args.state, countyShapes, mgrsToCountyMapping)
 
       // add the footprint to output file
-      writeFootprint(args.state, mgrsGrid, args.outputFolder, footprint)
+      writeFootprint(args.state, footprint.properties.mgrs, args.outputFolder, footprint)
     } catch (error) {
       console.log('error processing footprint', error, line)
       fpErrors++
