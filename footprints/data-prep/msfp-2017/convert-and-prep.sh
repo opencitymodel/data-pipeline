488928cf9d3bce6ddbd74570579e74f3faf165be
Allen Gilliland <agilliland@gmail.com>
2019-06-04 08:56:35 -0700

    update msfp-2017 scripts to filter out footprints where the Height value is null

--- footprints/data-prep/msfp-2017/convert-and-prep.sh
+++ footprints/data-prep/msfp-2017/convert-and-prep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MSFP_STATE=$1
-SOURCE_DIRECTORY=$2
-TARGET_DIRECTORY=$3
+SOURCE_DIRECTORY=$1
+TARGET_DIRECTORY=$2
+MSFP_STATE=$3
 
 if [[ "$MSFP_STATE" == *"California"* ]]; then
     echo "use convert-and-prep-California.sh instead"
@@ -17,7 +17,7 @@ cd ${TARGET_DIRECTORY}
 unzip ${SOURCE_DIRECTORY}/${MSFP_STATE}.zip
 
 # Delete the original zip to save disk space
-# rm -f ${MSFP_STATE}.zip
+rm -f ${MSFP_STATE}.zip
 
 # Convert shapefile into GeoJSON
 ogr2ogr -f "GeoJSON" ${MSFP_STATE}.geo.json bldg_footprints.shp bldg_footprints
@@ -26,7 +26,12 @@ ogr2ogr -f "GeoJSON" ${MSFP_STATE}.geo.json bldg_footprints.shp bldg_footprints
 rm -f bldg_footprints.*
 
 # Parse out extra JSON so we have just a single polygon per line
-grep '{ "type": "Feature", "properties": { "Height"' ${MSFP_STATE}.geo.json > ${MSFP_STATE}.txt
+grep '{ "type": "Feature", "properties": { "Height"' ${MSFP_STATE}.geo.json > ${MSFP_STATE}-all.txt
 
 # Delete the geojson file to save disk space
 rm -f ${MSFP_STATE}.geo.json
+
+# filter out the buildings with a null Height value, we are not going to use those for now
+grep 'Height": [0-9].' ${MSFP_STATE}-all.txt > ${MSFP_STATE}.txt
+
+#rm -f ${MSFP_STATE}-all.txt
