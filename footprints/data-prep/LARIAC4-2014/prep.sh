4d578741adf65ab4ba96b9393511e38a6725ee01
Allen Gilliland <agilliland@gmail.com>
2019-06-03 18:16:50 -0700

    fix up LARIAC4 data by adding a transform on the HEIGHT value from ft -> m

--- footprints/data-prep/LARIAC4-2014/prep.sh
+++ footprints/data-prep/LARIAC4-2014/prep.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 SOURCE_FILE=$1
-TARGET_DIRECTORY=$2
 
 
 ###  NOTE: this data only pertains to California
 
+LARIAC_DIR="LARIAC4"
 
-mkdir -p ${TARGET_DIRECTORY}
-cd ${TARGET_DIRECTORY}
+mkdir -p ${LARIAC_DIR}
 
 # Unzip
-unzip ${SOURCE_FILE}
+unzip ${SOURCE_FILE} -d ${LARIAC_DIR}
 
 # Convert gdb into GeoJSON
-ogr2ogr -f "GeoJSON" LA_county.geo.json LARIAC_Buildings_2014.gdb LARIAC4_BUILDINGS_2014
+ogr2ogr -f "GeoJSON" LA_county.geo.json ${LARIAC_DIR}/LARIAC_Buildings_2014.gdb LARIAC4_BUILDINGS_2014
 
 # Delete gdb stuff
-rm -f LARIAC_Buildings_2014.gdb "LARIAC4 Buildings (2014)*"
+rm -rf ${LARIAC_DIR}
 
 # Parse out GeoJSON scaffolding so we have a single Feature per line
-grep '{ "type": "Feature", "properties": { ' LA_county.geo.json > California.txt
+grep '{ "type": "Feature", "properties": { ' LA_county.geo.json > California-raw.txt
 
 # Delete the geojson file to save disk space
 rm -f LA_county.geo.json
 
-# NOTE: this source data is in EPSG:2229 so to use it there needs to be a CRS transform
+# transform CRS to WGS84 and adjust units from ft -> m
+node transform.js California-raw.txt California.txt 2229
+
+# finish tidy up
+rm -f California-raw.txt
